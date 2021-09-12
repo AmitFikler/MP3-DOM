8906637465c2be56c7c1278c2a9318449022309c
AmitFikler <amitfikler@gmail.com>
2021-09-12 16:33:03 +0300

    create element from js to dom, added some css

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,8 @@
  *
  * @param {String} songId - the ID of the song to play
  */
+
+
 function playSong(songId) {
     // Your code here
 }
@@ -12,9 +14,25 @@ function playSong(songId) {
  * Creates a song DOM element based on a song object.
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = []
-    const classes = []
+    const children = [{
+        content:title,
+        type:'p'
+    }, {
+        content:album,
+        type: 'p'
+    },{ 
+        content: artist,
+        type: 'p'
+    },{
+        content:  convertDuration(duration),
+        type: 'p'
+    }, {
+        content: coverArt,
+        type: 'img'
+    }]
+    const classes = ["songs", "continerSongs"]
     const attrs = { onclick: `playSong(${id})` }
+    
     return createElement("div", children, classes, attrs)
 }
 
@@ -22,8 +40,17 @@ function createSongElement({ id, title, album, artist, duration, coverArt }) {
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    const children = []
-    const classes = []
+    const children = [{
+        content:id,
+        type:'p'
+    }, {
+        content:name,
+        type: 'p'
+    },{ 
+        content: songs,
+        type: 'p'
+    }]
+    const classes = ["playlists", "continerSongs"]
     const attrs = {}
     return createElement("div", children, classes, attrs)
 }
@@ -41,7 +68,39 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-    // Your code here
+    let tag = document.createElement(tagName);
+    children.forEach(child => {
+        let type = document.createElement(child.type)
+        if (child.type = 'img') type.setAttribute("src", child.content);
+        type.innerHTML = child.content
+        tag.append(type)
+    });
+    classes.forEach(clas => {
+        tag.classList.add(clas)
+    })
+    for (const [key, value] of Object.entries(attributes)) {
+        tag.setAttribute(key,value)
+    }
+    document.body.appendChild(tag)
 }
 
 // You can write more code below this line
+function songList(songs) {
+    songs.forEach(song => {
+        createSongElement(song)       
+    });
+}
+
+function songPlayList(songs) {
+    songs.forEach(song => {
+        createPlaylistElement(song)       
+    });
+}
+songList(player.songs);
+songPlayList(player.playlists);
+
+function convertDuration(num) { //Converts seconds to MM:SS format
+    let mins = Math.floor(num / 60);
+    let sec = num % 60;
+    return mins.toString().padStart(2, '0') + ':' + sec.toString().padStart(2, '0');
+}
