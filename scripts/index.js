547fb08cdfa12d81d5261344dbe0ffec27db636d
AmitFikler <amitfikler@gmail.com>
2021-09-13 03:08:15 +0300

    added some functions for pass the task

--- scripts/index.js
+++ scripts/index.js
@@ -7,51 +7,46 @@
 
 
 function playSong(songId) {
-    // Your code here
-}
+    let song = document.getElementsByClassName()
 
+}
 /**
  * Creates a song DOM element based on a song object.
  */
 function createSongElement({ id, title, album, artist, duration, coverArt }) {
-    const children = [{
-        content:title,
-        type:'p'
-    }, {
-        content:album,
-        type: 'p'
-    },{ 
-        content: artist,
-        type: 'p'
-    },{
-        content:  convertDuration(duration),
-        type: 'p'
-    }, {
-        content: coverArt,
-        type: 'img'
-    }]
-    const classes = ["songs", "continerSongs"]
-    const attrs = { onclick: `playSong(${id})` }
-    
-    return createElement("div", children, classes, attrs)
+    const children = []
+    const classes = ["continerSongs", "songs"]
+    const ul = document.createElement("ul");
+    const img = document.createElement("img");
+    img.src = arguments[5]
+    for (let i = 1; i < arguments.length - 1; i++) {
+        if (i === 4) {
+            arguments[4] = convertDuration(arguments[4])
+        }
+        const li = document.createElement("li")
+        li.innerText = arguments[i] + "-";
+        ul.append(li)
+    }
+    ul.appendChild(img)
+    children.push(ul)
+    const attrs = { onclick: `playSong(${id})`, id: "song" + id}
+    return createElement("ul", children, classes, attrs)
 }
 
 /**
  * Creates a playlist DOM element based on a playlist object.
  */
 function createPlaylistElement({ id, name, songs }) {
-    const children = [{
-        content:id,
-        type:'p'
-    }, {
-        content:name,
-        type: 'p'
-    },{ 
-        content: songs,
-        type: 'p'
-    }]
-    const classes = ["playlists", "continerSongs"]
-    const attrs = {}
+    const children = []
+    const ul = document.createElement("ul")
+    for (let i = 1; i < arguments.length; i++) {
+        const li = document.createElement("li")
+        li.innerText = arguments[i]
+        ul.append(li)
+    }
+    children.push(ul)
+    const classes = ["playlists","continerSongs" ]
+    const attrs = {id: id}
     return createElement("div", children, classes, attrs)
 }
 
@@ -68,39 +63,96 @@ function createPlaylistElement({ id, name, songs }) {
  * @param {Object} attributes - the attributes for the new element
  */
 function createElement(tagName, children = [], classes = [], attributes = {}) {
-    let tag = document.createElement(tagName);
-    children.forEach(child => {
-        let type = document.createElement(child.type)
-        if (child.type = 'img') type.setAttribute("src", child.content);
-        type.innerHTML = child.content
-        tag.append(type)
-    });
-    classes.forEach(clas => {
-        tag.classList.add(clas)
-    })
-    for (const [key, value] of Object.entries(attributes)) {
-        tag.setAttribute(key,value)
+    const element = document.createElement(tagName);
+    for (let child of children) {
+        element.append(child);
     }
-    document.body.appendChild(tag)
+    for (let cls of classes) {
+        element.classList.add(cls);
+    }
+    for (let attr in attributes) {
+        element.setAttribute(attr, attributes[attr]);
+    }
+    return element;
 }
 
 // You can write more code below this line
-function songList(songs) {
-    songs.forEach(song => {
-        createSongElement(song)       
-    });
+
+function buildSongList(songs) {
+    const songList = []  
+    for (let song of songs) {
+        songList.push(song);
+        createSongElement(song)
+    }
+    songList.sort((a,b) => (a.title.toLowerCase() > b.title.toLowerCase()) ? 1 : -1);
+    return songList
 }
 
-function songPlayList(songs) {
-    songs.forEach(song => {
-        createPlaylistElement(song)       
-    });
+function buildPlaylistList(playlists){
+    const playlistList = []
+    for (let playlist of playlists) {
+        playlistList.push(playlist);
+        createPlaylistElement(playlist)
+    }
+    playlistList.sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1);
+    return playlistList
+
 }
-songList(player.songs);
-songPlayList(player.playlists);
 
 function convertDuration(num) { //Converts seconds to MM:SS format
     let mins = Math.floor(num / 60);
     let sec = num % 60;
     return mins.toString().padStart(2, '0') + ':' + sec.toString().padStart(2, '0');
 }
+
+function getSongById(id) { //Gets an ID and provides the details about the relevant song
+    for (let key of player.songs) {
+      if (key.id === id) {
+        return key;
+      }
+    }
+}
+function playlistDuration(id) {  // A function that returns the duration of all the songs in the playlist it receives
+    count = 0 ;
+    for (let song of player.playlists) {
+      if (song["id"] === id) {
+        for(let i of song["songs"])
+          count += getSongById(i)["duration"];
+      }
+    }
+    return count;
+}
+
+const songs = document.getElementById("songs")
+const playlists = document.getElementById("playlists")
+const siteHeader = document.createElement("header")
+const headerContent = document.createElement("h1")
+headerContent.innerText = `MP3 Player`
+const songHeader = document.createElement("h2")
+songHeader.innerText = `Songs`
+siteHeader.append(headerContent, songHeader)
+document.body.insertBefore(siteHeader, songs)
+const playlistHeader = document.createElement("h2")
+playlistHeader.innerText = `Playlists:`
+document.body.insertBefore(playlistHeader, playlists)
+
+function appendSong() {
+    buildSongList(player.songs).forEach((song) => {
+        const { id, title, album, artist, duration, coverArt } = song
+        const newSong = createSongElement(id, title, album, artist, duration, coverArt)
+        newSong.classList.add("song" + id)
+        songs.appendChild(newSong)
+    })
+} appendSong()
+
+function appendPlaylist() {
+    buildPlaylistList(player.playlists).forEach((playlist) => {
+        const { id, name, songs } = playlist
+        const newPlaylist = createPlaylistElement(id, name, songs)
+        playlists.append(newPlaylist)
+        const playListTime = document.createElement("li")
+        playListTime.innerText = convertDuration(playlistDuration(id))
+        newPlaylist.append(playListTime)
+    })
+}
+appendPlaylist()
